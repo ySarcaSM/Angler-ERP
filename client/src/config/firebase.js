8d6f6fa87f5c5189b4dfa7b30d65276210fa1715
ySarcaSM <73149903+ySarcaSM@users.noreply.github.com>
2026-09-22 09:25:52 -0300

    fix: stabilize Firestore WebChannel transport

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -5,7 +5,7 @@
 // https://console.firebase.google.com
 
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
@@ -20,7 +20,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export default app;
